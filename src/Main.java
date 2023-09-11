b2356db202622856f1f74555cc4c00e09138a72e
Macy Chen <macychen@Macys-MacBook-Pro.local>
2023-09-11 15:08:50 -0400

    Edit to comments in Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         FizzBuzz.doFizzBuzz(5);
 
-        //Task 3.2 - must be padded String[] args when calling it
+        //Task 3.2 - String[] args must be passed when calling main()
         FizzBuzz.main(null);
     }
 }
