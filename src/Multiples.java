e2a7bf017eb73d2a06036c26a006ecad3908a200
Macy Chen <macychen@Macys-MacBook-Pro.local>
2023-09-11 19:47:30 -0400

    Changed to number of multiples below 1000

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,7 +1,7 @@
 public class Multiples {
     public static void main(String[] args) {
         int counter = 0;
-        for (int i = 1; i < 10; i++) {
+        for (int i = 1; i < 1000; i++) {
             boolean multipleOf3 = i % 3 == 0;
             boolean multipleOf5 = i % 5 == 0;
 
