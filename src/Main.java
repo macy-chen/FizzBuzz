c3a07b627d4b3cfaff3b251fdf055efc33e06599
Macy Chen <macychen@Macys-MacBook-Pro.local>
2023-09-11 15:01:29 -0400

    Added Main.java

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        FizzBuzz.doFizzBuzz(5);
+
+        //Task 3.2 - must be padded String[] args when calling it
+        FizzBuzz.main(null);
+    }
+}
